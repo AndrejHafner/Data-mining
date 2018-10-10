0b7df4ce5b2120eea824131e6f0790504ebca5c9
Andrej Hafner <andrej.hafn3r@gmail.com>
2018-10-10 15:59:52 +0200

    Started with data preprocessing

--- Naloga_1-Eurovision/naloga1.py
+++ Naloga_1-Eurovision/naloga1.py
@@ -1,25 +1,41 @@
 import csv
 import numpy as np
 
+REMOVED_ATTRIBUTES = ['Region', 'Song language', 'Artist', 'Song', 'English translation', 'Artist gender', 'Group/Solo', 'Place', 'Points', 'Host Country', 'Host region', 'Home/Away Country', 'Home/Away Region', 'Approximate Betting Prices']
+
 def read_file(file_name):
     """
     Read and process data to be used for clustering.
     :param file_name: name of the file containing the data
     :return: dictionary with element names as keys and feature vectors as values
     """
+    # Open the file and read its contents to a list
     file = open(file_name, "rt", encoding="latin1")
     table = []
-
     reader = csv.reader(file)
     for line in reader:
         table.append(line)
 
+    # Clean the keys and create a dictionary
+    table[0] = list(map(lambda x : str(x).strip(),table[0]))
     data_dict = dict.fromkeys(table[0])
-    
-    print(data_dict.keys())
-    # for i in range(len(table[0])):
-    #     print(table[0][i])
 
+    # Insert all values into the dictionary
+    k = 0
+    for key in data_dict.keys():
+        for i in range(1,len(table)):
+            data_dict[key] = [table[i][k]] if data_dict[key] == None else data_dict[key] + [table[i][k]]
+        k += 1
+    data_dict.pop('',None)
+
+    # Remove attributes that are not needed
+    for attr in REMOVED_ATTRIBUTES:
+        data_dict.pop(attr,None)
+
+    for key in data_dict.keys():
+        print(data_dict[key])
+
+    return data_dict
 
 
 
@@ -81,6 +97,6 @@ class HierarchicalClustering:
 if __name__ == "__main__":
     DATA_FILE = "eurovision-final.csv"
     read_file(DATA_FILE)
-    # hc = HierarchicalClustering(read_file(DATA_FILE))
-    # hc.run()
-    # hc.plot_tree()
+    hc = HierarchicalClustering(read_file(DATA_FILE))
+    hc.run()
+    hc.plot_tree()
